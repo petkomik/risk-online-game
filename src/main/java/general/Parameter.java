75ba5bc5ecff338a915fd8b4f94585a77e710f90
Johannes Rohr <jorohr@mail.uni-mannheim.de>
2023-03-13 13:41:03 +0100

    #15 Create Parameter class

---
+++ src/main/java/general/Parameter.java
@@ -0,0 +1,12 @@
+package general;
+
+public class Parameter {
+
+	public static final String sep = System.getProperty("file.separator");
+	public static final String userdir = System.getProperty("user.dir");
+	public static final String resourcesdir = userdir + sep + "resources" + sep;
+	
+	public static final String dbName = "risiko-database.db";
+	
+}
+
