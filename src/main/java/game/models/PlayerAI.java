90d0a02ed84ac13fd0f6095e84fc8d8778f7aeb8
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-03-14 00:01:02 +0100

    #11 added constructors to the child classes of player

--- src/main/java/game/models/PlayerAI.java
+++ src/main/java/game/models/PlayerAI.java
@@ -1,6 +1,12 @@
 //@author Stefan Rogalski
 package game.models;
 
+import java.time.LocalTime;
+
 public class PlayerAI extends Player {
+
+	public PlayerAI(int id, int wins, String name, String color, String picturePath, LocalTime gameTime) {
+		super(id, wins, name, color, picturePath, gameTime, false);
+	}
 
 }
--- src/main/java/game/models/PlayerMP.java
+++ src/main/java/game/models/PlayerMP.java
@@ -2,6 +2,14 @@
 
 package game.models;
 
+import java.time.LocalTime;
+
 public class PlayerMP extends Player {
+
+	public PlayerMP(int id, int wins, String name, String color, String picturePath, LocalTime gameTime,
+			boolean isPersonal) {
+		super(id, wins, name, color, picturePath, gameTime, isPersonal);
+		// TODO Auto-generated constructor stub
+	}
 
 }
