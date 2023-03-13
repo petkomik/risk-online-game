1200415290a9c57e47f6d605cbde0985e4cb6e8a
Johannes Rohr <jorohr@mail.uni-mannheim.de>
2023-03-13 20:36:15 +0100

    #15 update Parameter with respect to the Java Style Guide

--- src/main/java/general/Parameter.java
+++ src/main/java/general/Parameter.java
@@ -1,4 +1,5 @@
 package general;
+
 /**
  * Parameter class to get to directories, files, the database, etc.
  *
@@ -9,8 +10,7 @@ public class Parameter {
 
 	public static final String sep = System.getProperty("file.separator");
 	public static final String resourcesdir = "/risk5/src/main/resources" + sep;
-	
-	public static final String dbName = "risiko-database.db";
-	
+	public static final String dbName = "risk-database.db";
+
 }
 
