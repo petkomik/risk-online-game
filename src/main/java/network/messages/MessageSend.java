9206627c33f9b1755485a06e4c4210e0800da2ad
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-03-14 19:02:37 +0100

    #18 edited MessageSend

--- src/main/java/network/messages/MessageSend.java
+++ src/main/java/network/messages/MessageSend.java
@@ -1,11 +1,18 @@
 package network.messages;
 
+import game.models.Player;
+
 public class MessageSend extends Message {
 	
 	private static final long serialVersionUID = 1L;
+	
+	private String message;
+	private Player player;
 
-	public MessageSend() {
+	public MessageSend(String message, Player player) {
 		super(MessageType.MessageSend);
+		this.message = message;
+		this.player = player;
 	}
 
 }
