bc69941fd56a7d7d24b4c8f6f2ccdbd894260dab
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-03-14 19:39:54 +0100

    #11 added constructor and get/set in territory class

--- src/main/java/game/models/Territory.java
+++ src/main/java/game/models/Territory.java
@@ -1,7 +1,7 @@
 package game.models;
 
 /**
- * 
+ * Territory class handles the territories in the game as an object
  * @author srogalsk
  *
  */
@@ -12,5 +12,34 @@ public class Territory {
 	private Continent continent;
 	private int numberOfTroops;
 	private CountryName countryName;
+	
+	public Territory(CountryName countryName, Continent continent) {
+		this.countryName = countryName;
+		this.continent = continent;
+	}
+	
+	
+	
+	/**Getters*/
+	public Player getOwnedByPlayer() {
+		return ownedByPlayer;
+	}
+	public int getNumberOfTroops() {
+		return numberOfTroops;
+	}
+	public Continent getContinent() {
+		return continent;
+	}
+	public CountryName getCountryName() {
+		return countryName;
+	}
+	
+	/**Setters*/
+	public void setNumberOfTroops(int numberOfTroops) {
+		this.numberOfTroops = numberOfTroops;
+	}
+	public void setOwnedByPlayer(Player ownedByPlayer) {
+		this.ownedByPlayer = ownedByPlayer;
+	}
 	
 }
