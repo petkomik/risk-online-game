effce34becee76759d104163d68aa07b190e2022
Johannes Rohr <jorohr@mail.uni-mannheim.de>
2023-03-14 13:00:52 +0100

    #15 updated Parameter class with actual path

--- src/main/java/general/Parameter.java
+++ src/main/java/general/Parameter.java
@@ -9,7 +9,8 @@ package general;
 public class Parameter {
 
 	public static final String sep = System.getProperty("file.separator");
-	public static final String resourcesdir = "/risk5/src/main/resources" + sep;
+	public static final String userdir = System.getProperty("user.dir");
+	public static final String resourcesdir = userdir + sep + "src/main/resources/";
 	public static final String dbName = "risk-database.db";
 
 }
