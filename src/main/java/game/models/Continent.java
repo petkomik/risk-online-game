e9cc17bc8b55b53ea10553623010c0c3fd9e8ea1
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-03-14 19:29:56 +0100

    #11 continent enum added

---
+++ src/main/java/game/models/Continent.java
@@ -0,0 +1,18 @@
+package game.models;
+
+/**
+ * Enum for all Continents on the GameField
+ * @author srogalsk
+ *
+ */
+
+public enum Continent {
+	
+	Africa,
+	Asia,
+	Australia,
+	Europe,
+	NorthAmerica,
+	SouthAmerica;
+
+}
