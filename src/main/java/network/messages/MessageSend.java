2180306a97ffcc7a504abab04728fe4d71abb67f
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-03-14 12:35:32 +0100

    #18 added MessageSend

---
+++ src/main/java/network/messages/MessageSend.java
@@ -0,0 +1,11 @@
+package network.messages;
+
+public class MessageSend extends Message {
+	
+	private static final long serialVersionUID = 1L;
+
+	public MessageSend(MessageType mType) {
+		super(mType);
+	}
+
+}
--- src/main/java/network/messages/MessageType.java
+++ src/main/java/network/messages/MessageType.java
@@ -3,5 +3,5 @@ package network.messages;
 public enum MessageType {
 	Connect,
 	Disconnect,
-	Message;
+	MessageSend;
 }
