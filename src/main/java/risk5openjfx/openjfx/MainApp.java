6a9459b732b6fe9c46ad9da75b793d9c4795730f
Majd Alaji <malaji@mail.uni-mannheim.de>
2023-03-14 21:58:59 +0100

    Fixes #19 Maximize the stage and make it nonresizable

--- src/main/java/risk5openjfx/openjfx/MainApp.java
+++ src/main/java/risk5openjfx/openjfx/MainApp.java
@@ -4,18 +4,29 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainApp extends Application{
 	
+	private AnchorPane anchorPane;
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+    	// Getting the anchor pane from the file main.fxml
+    	anchorPane = (AnchorPane) loadFXML("main");
+    	// Setting the anchor pane as a root into the scene
+        scene = new Scene(anchorPane);
+        // Setting the scene into the stage
         stage.setScene(scene);
+        // Setting the stage on the maximized mode
+        stage.setMaximized(true);
+        // Setting the resizability of the stage on false
+        stage.setResizable(false);
+        // Showing the stage
         stage.show();
     }
 
