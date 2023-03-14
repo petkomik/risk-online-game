c5ecbc65f4ba89a95bd9e0c1f67429910a350265
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-03-14 19:29:05 +0100

    #11 added Enum CountryName

---
+++ src/main/java/game/models/CountryName.java
@@ -0,0 +1,51 @@
+package game.models;
+
+	/**
+	 * enum for country/territory names
+	 * @author srogalsk
+	 *
+	 */
+
+public enum CountryName {
+	Alaska,
+	Alberta,
+	NorthwestTerritory,
+	Ontario,
+	Quebec,
+	WesternUnitedStates,
+	EasternUnitedStates,
+	CentralAmerica,
+	Venezuela,
+	Brazil,
+	Peru,
+	Argentinia,
+	Iceland,
+	Scandinavia,
+	GreatBritain,
+	NorthernEurope,
+	WesternEurope,
+	SouthernEurope,
+	Ukraine,
+	Ural,
+	Siberia,
+	Yakutsk,
+	Kamchatka,
+	Irkutsk,
+	Mongolia,
+	Afghanistan,
+	Japan,
+	China,
+	Siam,
+	India,
+	MiddleEast,
+	Indonesia,
+	NewGuinea,
+	EasternAustralia,
+	WesternAustralia,
+	NorthAfrica,
+	Egypt,
+	EastAfrica,
+	Congo,
+	Madagascar,
+	SouthAfrica,
+}
