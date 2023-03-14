41c7b8adae6519907b076155d3158e4e5dd9dcd4
Johannes Rohr <jorohr@mail.uni-mannheim.de>
2023-03-14 15:06:47 +0100

    #17 test to create a player profile in database

--- src/main/java/database/TestDB.java
+++ src/main/java/database/TestDB.java
@@ -1,5 +1,9 @@
 package database;
 
+import java.time.LocalTime;
+
+import game.models.Player;
+
 /**
  * class to test the database.
  *
@@ -9,5 +13,8 @@ public class TestDB {
 	public static void main(String[]args) {
 		CreateDatabase db = new CreateDatabase();
 		db.createTables();
+		DatabaseHandler dbH = new DatabaseHandler();
+		Player p1 = new Player(11111, 4, "Max", "blue", "C:\\user\\exmaplePath\\furtherExample\\photo.jpg", LocalTime.now(), true);
+		dbH.createPlayerData(p1);
 	}
 }
