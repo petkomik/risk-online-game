7b8f3368126d6c964a58aed4c80f9dbac0dc7f91
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-03-14 19:30:27 +0100

    #11 territory class added

---
+++ src/main/java/game/models/Territory.java
@@ -0,0 +1,16 @@
+package game.models;
+
+/**
+ * 
+ * @author srogalsk
+ *
+ */
+
+public class Territory {
+	
+	private Player ownedByPlayer;
+	private Continent continent;
+	private int numberOfTroops;
+	private CountryName countryName;
+	
+}
