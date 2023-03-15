7294d5bfad1ec7c0c3919b04bbe77136f353a827
Majd Alaji <malaji@mail.uni-mannheim.de>
2023-03-15 23:48:10 +0100

    .#15 Add logoImage

--- src/main/java/general/Parameter.java
+++ src/main/java/general/Parameter.java
@@ -11,6 +11,7 @@ public class Parameter {
 	public static final String sep = System.getProperty("file.separator");
 	public static final String userdir = System.getProperty("user.dir");
 	public static final String resourcesdir = userdir + sep + "src" + sep + "main" + sep + "resources" + sep;
+	public static final String logoImage = resourcesdir+sep+"risk5openjfx"+sep+"openjfx"+sep+"transparent-risk.png";
 	public static final String dbName = "risk-database.db";
 
 }
