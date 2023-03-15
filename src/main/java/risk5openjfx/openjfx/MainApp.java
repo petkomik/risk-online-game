f89409171594d17fdff53d67a41db1cc00da99e5
Majd Alaji <malaji@mail.uni-mannheim.de>
2023-03-15 10:12:00 +0100

    .#19 Set stage logo

--- src/main/java/risk5openjfx/openjfx/MainApp.java
+++ src/main/java/risk5openjfx/openjfx/MainApp.java
@@ -4,22 +4,34 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+/**
+ * 
+ * @author malaji
+ * This class shows the very first page
+ */
 public class MainApp extends Application{
 	
 	private AnchorPane anchorPane;
     private Scene scene;
-
+    /**
+     * This method initializes the required elements
+     */
     @Override
     public void start(Stage stage) throws IOException {
     	// Getting the anchor pane from the file main.fxml
     	anchorPane = (AnchorPane) loadFXML("main");
     	// Setting the anchor pane as a root into the scene
         scene = new Scene(anchorPane);
+        // Getting the pic transparent-risk.png
+        Image image = new Image("/risk5/src/main/resources/risk5openjfx/openjfx/transparent-risk.png");
+        // Setting the pic transparent-risk.png as logo of the stage
+        stage.getIcons().add(image);
         // Setting the scene into the stage
         stage.setScene(scene);
         // Setting the stage on the maximized mode
