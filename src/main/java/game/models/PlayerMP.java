51d29d21facb7fa23606c8c82dd4298f992c7052
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-03-15 11:53:43 +0100

    #11 changed attribute isme to false

--- src/main/java/game/models/PlayerMP.java
+++ src/main/java/game/models/PlayerMP.java
@@ -6,10 +6,8 @@ import java.time.LocalTime;
 
 public class PlayerMP extends Player {
 
-	public PlayerMP(int id, int wins, String name, String color, String picturePath, LocalTime gameTime,
-			boolean isPersonal) {
-		super(id, wins, name, color, picturePath, gameTime, isPersonal);
-		// TODO Auto-generated constructor stub
+	public PlayerMP(int id, int wins, String name, String color, String picturePath, LocalTime gameTime) {
+		super(id, wins, name, color, picturePath, gameTime, false);
 	}
 
 }
