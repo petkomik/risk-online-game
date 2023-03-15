1a4be8b9a6df58fbab796b0461287aef68fffd0a
Majd Alaji <malaji@mail.uni-mannheim.de>
2023-03-15 23:42:13 +0100

    .#15 Change resourcesdir

--- src/main/java/general/Parameter.java
+++ src/main/java/general/Parameter.java
@@ -10,7 +10,7 @@ public class Parameter {
 
 	public static final String sep = System.getProperty("file.separator");
 	public static final String userdir = System.getProperty("user.dir");
-	public static final String resourcesdir = userdir + sep + "src/main/resources/";
+	public static final String resourcesdir = userdir + sep + "src" + sep + "main" + sep + "resources";
 	public static final String dbName = "risk-database.db";
 
 }
