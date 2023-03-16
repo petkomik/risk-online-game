61795c1323ec77dd7b01ede3a9cdad2d2760940a
Majd Alaji <malaji@mail.uni-mannheim.de>
2023-03-16 11:39:18 +0100

    .#20 Changed file module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module risk5openjfx.openjfx {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+	requires javafx.base;
 
     opens risk5openjfx.openjfx to javafx.fxml;
     exports risk5openjfx.openjfx;
