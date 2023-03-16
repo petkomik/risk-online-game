8f6f91c3fcda9b127a6ade05a0ccafc0d4f59fbe
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-03-16 19:04:17 +0100

    #15 changed logoImagePath

--- src/main/java/general/Parameter.java
+++ src/main/java/general/Parameter.java
@@ -12,7 +12,7 @@ public class Parameter {
 	public static final String userdir = System.getProperty("user.dir");
 	public static final String resourcesdir = userdir + sep + "src" + sep + "main" + sep + "resources" + sep;
 	public static final String imagesdir = resourcesdir + "images" + sep;
-	public static final String logoImage = resourcesdir+sep+"risk5openjfx"+sep+"openjfx"+sep+"transparent-risk.png";
+	public static final String logoImage = imagesdir + "transparent-risk.png";
 	public static final String dbName = "risk-database.db";
 
 }
