cfa12cb7c27a137b02ae8066cd95a0ce63faac7a
Majd Alaji <malaji@mail.uni-mannheim.de>
2023-03-16 11:38:23 +0100

    .#21 Added method clickplay(ActionEvent e)

---
+++ src/main/java/risk5openjfx/openjfx/StartPaneController.java
@@ -0,0 +1,30 @@
+package risk5openjfx.openjfx;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class StartPaneController {
+	private Stage stage;
+	private AnchorPane anchorPane;
+	
+	public void clickPlay(ActionEvent e) throws IOException {
+		Node node = (Node)e.getSource();
+		stage = (Stage)node.getScene().getWindow();
+		anchorPane = (AnchorPane) loadFXML("createProfile");
+		stage.getScene().setRoot(anchorPane);
+		stage.show();
+
+	}
+	
+	private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartPaneController.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+	}
+    
+}
