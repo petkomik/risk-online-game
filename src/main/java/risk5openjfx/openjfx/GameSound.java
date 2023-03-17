eadf9443117e0449a1e3f1d7135eea0e29c66a66
Petko Mikov <pmikov@mail.uni-mannheim.de>
2023-03-17 11:36:17 +0100

    #24 commented imports to remove errors

--- src/main/java/risk5openjfx/openjfx/GameSound.java
+++ src/main/java/risk5openjfx/openjfx/GameSound.java
@@ -7,8 +7,9 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
+//import javafx.scene.media.Media;
+//import javafx.scene.media.MediaPlayer;
+
 
 public class GameSound {
 
