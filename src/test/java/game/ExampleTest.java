dfa56c90cee3a4298e08d8c445611701cb612423
Petko Mikov <pmikov@mail.uni-mannheim.de>
2023-03-17 14:11:38 +0100

    #28 added JUnit dependencies to pom and created an example test class

---
+++ src/test/java/game/ExampleTest.java
@@ -0,0 +1,14 @@
+//@author Petko Mikov
+package game;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class JUnit5ExampleTest {
+	 
+    @Test
+    void justAnExample() {
+    	Lobby testLobby = new Lobby();
+    	assertTrue(testLobby.getLobbyRank() == 0);
+    }	
+}
