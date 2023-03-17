f87488b464a25b9b355d6949c307f33289a2b6c9
Johannes Rohr <jorohr@mail.uni-mannheim.de>
2023-03-17 13:16:10 +0100

    #11 Player changed Player structure (+ changed to abstract)

--- src/main/java/game/models/Player.java
+++ src/main/java/game/models/Player.java
@@ -2,6 +2,9 @@ package game.models;
 
 import java.time.LocalTime;
 
+import game.GameState;
+import game.Profile;
+
 /**
  * Player class to model the player entity
  *
@@ -9,54 +12,14 @@ import java.time.LocalTime;
  */
 
 public class Player {
-	private int id;
-	private int wins;
 	
-	private String name;
+	private GameState gameState;
+	
 	private String color;
-	private String picturePath;
-	private LocalTime gameTime;
+	private String name;
 	
-	private boolean isPersonal;
-	
-	public Player(int id, int wins, String name, String color, String picturePath, LocalTime gameTime,
-			boolean isPersonal) {
-		super();
-		this.id = id;
-		this.wins = wins;
-		this.name = name;
-		this.color = color;
-		this.picturePath = picturePath;
-		this.gameTime = gameTime;
-		this.isPersonal = isPersonal;
+	public Player() {
+		
 	}
 	
-	public int getId() {
-		return id;
-	}
-
-	public int getWins() {
-		return wins;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getColor() {
-		return color;
-	}
-
-	public String getPicturePath() {
-		return picturePath;
-	}
-
-	public LocalTime getGameTime() {
-		return gameTime;
-	}
-
-	public boolean isPersonal() {
-		return isPersonal;
-	}
-
 }
