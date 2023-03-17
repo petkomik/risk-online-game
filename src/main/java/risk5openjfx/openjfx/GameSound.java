9ac0a3335ef72e68edbf1809c675f36168155d66
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-03-17 10:47:57 +0100

    #24 GameSound class with unrecognized imports

---
+++ src/main/java/risk5openjfx/openjfx/GameSound.java
@@ -0,0 +1,15 @@
+package risk5openjfx.openjfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+public class GameSound {
+
+}
