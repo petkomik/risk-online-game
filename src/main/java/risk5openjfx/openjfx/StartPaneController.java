9e48721ef6c2354478c03115dc31a0b68608d722
Majd Alaji <malaji@mail.uni-mannheim.de>
2023-03-18 17:22:40 +0100

    Added describing comments for the methods #21

--- src/main/java/risk5openjfx/openjfx/StartPaneController.java
+++ src/main/java/risk5openjfx/openjfx/StartPaneController.java
@@ -8,11 +8,19 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
+/**
+ * 
+ * @author majda
+ * this class handles the events on the very first frame
+ */
 public class StartPaneController {
 	private Stage stage;
 	private AnchorPane anchorPane;
-	
+	/**
+	 * this method shows the create profile frame after the player has clicked the button 'play'
+	 * @param e
+	 * @throws IOException
+	 */
 	public void clickPlay(ActionEvent e) throws IOException {
 		
 		(new GameSound()).buttonClickForwardSound();
@@ -24,7 +32,14 @@ public class StartPaneController {
 		stage.show();
 
 	}
-	
+	/**
+     * 
+     * @param fxml, file name without the ending .fxml
+     * @return Parent object, to be set as a root in a Secene object
+     * @throws IOException
+     * 
+     * This method is responsible for loading a fxml file
+     */
 	private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartPaneController.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
