a748bcd85e232742de477d99d274bb90d723b037
Johannes Rohr <jorohr@mail.uni-mannheim.de>
2023-03-21 18:02:47 +0100

    #14 merged CreateDatabase with Database

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -20,6 +20,7 @@ public class Database {
 		this.connect(general.Parameter.resourcesdir + general.Parameter.dbName);
 		properties = new Properties();
 		properties.setProperty("PRAGMA foreign_keys", "ON");
+		this.createTables();
 	}
 
 	private void connect(String file) {
@@ -53,4 +54,52 @@ public class Database {
 			e.printStackTrace();
 		}
 	}
+	
+	public void createTables() {
+		this.createTable_Profiles();
+		this.createTable_Games();
+	}
+
+	private void createTable_Profiles() {
+		try (Statement stm = connection.createStatement()){
+			String sql = "CREATE TABLE IF NOT EXISTS Profiles "
+					+ "(PlayerID INTGEGER PRIMARY KEY, "
+					+ "UserName TEXT NOT NULL, "
+					+ "FirstName TEXT NOT NULL, "
+					+ "LastName TEXT NOT NULL, "
+					+ "Wins INTEGER, "
+					+ "Color TEXT,"
+					+ "Photo TEXT, "
+					+ "Loses INTEGER, "
+					+ "Password TEXT NOT NULL, "
+					+ "Score INTEGER, "
+					+ "IsPersonal BIT NOT NULL);";
+			stm.executeUpdate(sql);
+			connection.commit();
+			
+			System.out.println("Table Players was created...");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
+	private void createTable_Games() {
+		try (Statement stm = connection.createStatement()){
+			String sql = "CREATE TABLE IF NOT EXISTS Games "
+					+ "(GameID INTEGER PRIMARY KEY, "
+					+ "Length INTEGER NOT NULL, "
+					+ "StartTime INTEGER NOT NULL, "
+					+ "NumberOfPlayers INTEGER NOT NULL, "
+					+ "WinnerID INTEGER NOT NULL REFERENCES Players);";
+			stm.executeUpdate(sql);
+			connection.commit();
+			
+			System.out.println("Table Games was created...");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+
 }
--- src/main/java/game/GameController.java
+++ src/main/java/game/GameController.java
@@ -12,13 +12,10 @@ import database.PlayerProfileHandler;
 public class GameController {
 	
 	private Profile profile;
-	private static CreateDatabase db;
 	
 	public boolean createFirstProfile(String userName, String firstName, String lastName,  String password) {
 		try {
 		profile = new Profile(firstName, lastName, userName, password);
-		db = new CreateDatabase();
-		db.createTables();
 		PlayerProfileHandler dbH = new PlayerProfileHandler();
 		dbH.createProfileData(profile);
 		return true;
