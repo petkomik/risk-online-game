6ede109ebe7d23c67bdc2e8bddaa7e8d5ebc9b17
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-03-24 23:23:17 +0100

    #32 HostServerMessengerFrame created but not finished yet

---
+++ src/main/java/game/gui/HostServerMessengerController.java
@@ -0,0 +1,126 @@
+package game.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import network.Server;
+
+public class HostServerMessengerController implements Initializable {
+	
+	private double w = MainApp.screenWidth;
+	private double h = MainApp.screenHeight;
+	
+	@FXML
+	private Button sendButton;
+	@FXML
+	private TextField textFieldMessage;
+	@FXML
+	private VBox vBoxMessages;
+	@FXML
+	private ScrollPane scrollPaneMain;
+	
+	private Server server;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		sendButton.setPrefSize(w * 0.091, h * 0.058);
+		textFieldMessage.setPrefSize(w * 0.400, h * 0.100);
+		vBoxMessages.setPrefSize(w * 0.391, h * 0.358);
+		scrollPaneMain.setPrefSize(w * 0.400, h * 0.370);
+
+		this.setXYof(0.430, 0.200, sendButton);
+		this.setXYof(0.439, 0.800, textFieldMessage);
+		this.setXYof(0.430, 0.301, scrollPaneMain);
+		
+		/******/
+		
+		vBoxMessages.heightProperty().addListener(new ChangeListener<Number>() {
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+				scrollPaneMain.setVvalue((Double) newValue);
+			}
+		});
+		
+		sendButton.setOnAction(new EventHandler<ActionEvent>() {
+			
+			@Override
+			public void handle(ActionEvent event) {
+				String messageToSend = textFieldMessage.getText();
+				if(!messageToSend.isBlank()) {
+					HBox hBox = new HBox();
+					hBox.setAlignment(Pos.CENTER_RIGHT);
+					hBox.setPadding(new Insets(5,5,5,10));
+					Text text = new Text(messageToSend);
+					TextFlow textFlow = new TextFlow(text);
+					textFlow.setStyle("-fx-color: rgb(239,242,255) "
+							+ "-fx-background-color: rgb(15,125,242) "
+							+ "-fx-background-radius: 20px");
+					textFlow.setPadding(new Insets(5,10,5,10));
+					text.setFill(Color.color(0.934,0.945,0.996));
+					
+					hBox.getChildren().add(textFlow);
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							vBoxMessages.getChildren().add(hBox);
+						}
+					});
+					
+					// server.sendMessage(message);
+					
+					textFieldMessage.clear();
+					
+				}
+			}
+		});
+		
+		
+	}
+
+	public static void addLabel(String messageFromCLient, VBox vBox) {
+		HBox hBox = new HBox();
+		hBox.setAlignment(Pos.CENTER_LEFT);
+		hBox.setPadding(new Insets(5,5,5,10));
+		
+		Text text = new Text(messageFromCLient);
+		TextFlow textFlow = new TextFlow(text);
+		textFlow.setStyle("-fx-color: rgb(239,242,255) "
+				+ "-fx-background-color: rgb(233,233,235) "
+				+ "-fx-background-radius: 20px");
+		textFlow.setPadding(new Insets(5,10,5,10));
+		hBox.getChildren().add(textFlow);
+		
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				vBox.getChildren().add(hBox);
+			}
+		});
+		
+	}
+	
+	private void setXYof(double relativeX, double relativeY, Node node) {
+		node.setLayoutX(w * relativeX);
+		node.setLayoutY(h * relativeY);
+	}
+
+}
--- src/main/java/game/gui/MultplayerHostJoinController.java
+++ src/main/java/game/gui/MultplayerHostJoinController.java
@@ -1,24 +1,32 @@
 package game.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import general.Parameter;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 
 public class MultplayerHostJoinController implements Initializable {
 	private Stage stage;
 	private AnchorPane anchorPane;
 	private double w = MainApp.screenWidth;
 	private double h = MainApp.screenHeight;
-	
+
 	@FXML
 	private Button hostGameButton;
 	@FXML
 	private Button joinGameButton;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		double btnW = w * 0.163;
@@ -26,17 +34,43 @@ public class MultplayerHostJoinController implements Initializable {
 		double btnX = w * 0.419;
 		hostGameButton.setPrefSize(btnW, btnH);
 		joinGameButton.setPrefSize(btnW, btnH);
-		
+
 		hostGameButton.setLayoutX(btnX);
 		joinGameButton.setLayoutX(btnX);
 
-		hostGameButton.setLayoutY(h*0.301);
-		joinGameButton.setLayoutY(h*0.382);	
+		hostGameButton.setLayoutY(h * 0.301);
+		joinGameButton.setLayoutY(h * 0.382);
+
+		double fontSize = 0.078 * Math.sqrt(Math.pow(btnW, 2.0) + Math.pow(btnH, 2.0));
+		hostGameButton.setStyle("-fx-font-size: " + fontSize + "px;");
+		joinGameButton.setStyle("-fx-font-size: " + fontSize + "px;");
+
+	}
+
+	public void startServer(ActionEvent e) throws IOException {
+
+		(new GameSound()).buttonClickForwardSound();
+
+		Node node = (Node) e.getSource();
+		// Getting the Stage where the event is happened
+		stage = (Stage) node.getScene().getWindow();
+		// changing the AnchorPane from the main file
+		anchorPane = (AnchorPane) loadFXML("HostServerMessengerFrame");
+		// Setting the size of the anchorPane
+		anchorPane.setPrefSize(w, h);
+		// Setting the AnchorPane as a root of the main scene
+		stage.getScene().setRoot(anchorPane);
+		// Showing the Stage
+		stage.show();
+	}
+	
+	public void joinServer(ActionEvent e) throws IOException {
 		
-		double fontSize = 0.078 * Math.sqrt(Math.pow(btnW, 2.0)+Math.pow(btnH, 2.0));
-		hostGameButton.setStyle("-fx-font-size: "+fontSize+"px;");
-		joinGameButton.setStyle("-fx-font-size: "+fontSize+"px;");
-		
+	}
+	
+	private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartPaneController.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
 	}
 
 }
