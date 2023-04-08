b9abbf5c4c3c1b91706de672dfc146bf13805793
Majd Alaji <malaji@mail.uni-mannheim.de>
2023-04-08 12:19:20 +0200

    Added all countries and created a handler class #37

---
+++ src/main/java/game/gui/GamePaneController.java
@@ -0,0 +1,26 @@
+package game.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+
+import javafx.fxml.Initializable;
+
+
+public class GamePaneController implements Initializable{
+	
+	private double w = MainApp.screenWidth;
+	private double h = MainApp.screenHeight;
+	
+	
+	
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+	}
+
+
+}
