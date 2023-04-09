5cd41103b42f68ef222fc9373c0b53c5994674d3
Majd Alaji <malaji@mail.uni-mannheim.de>
2023-04-09 18:44:22 +0200

    Changed the setting of the scale and the position of the countries #37

--- src/main/java/game/gui/GamePaneController.java
+++ src/main/java/game/gui/GamePaneController.java
@@ -3,9 +3,11 @@ package game.gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-
-
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.SVGPath;
 
 
 public class GamePaneController implements Initializable{
@@ -15,11 +17,19 @@ public class GamePaneController implements Initializable{
 	
 	
 	
-	
+	@FXML
+	private AnchorPane gameBoard;
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-
+		for(Node n : gameBoard.getChildren()) {
+			if(n instanceof SVGPath) {
+				n.setScaleX(w*(0.2/1536));
+				n.setScaleY(h*(-0.2/864));
+				n.setLayoutX(w*(n.getLayoutX()/1536));
+				n.setLayoutY(h*(n.getLayoutY()/864));
+			}
+		}
 	}
 
 
