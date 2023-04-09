256bcc07367253758ed27f7bdbb1ae3fcef3f015
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-04-09 18:52:13 +0200

    #29 adedd updateOwnedContinents for Player

--- src/main/java/game/models/Player.java
+++ src/main/java/game/models/Player.java
@@ -1,6 +1,7 @@
 package game.models;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Player class to model the player entity
@@ -14,13 +15,13 @@ public class Player {
 	private String color;
 	private int id;
 	private int rank;
-	private ArrayList<Territory> ownedCountries;
+	private HashMap<CountryName, Territory> ownedCountries;
 	private ArrayList<Continent> ownedContinents;
 	private int sumOfAllTroops;
 	private int troopsAvailable;
-	private int numberOfCardsTurnedIn;
 	private ArrayList<Card> cards;
-
+	private boolean canContinuePlaying = true;
+	private volatile boolean cardsTurningInPhase;
 	
 	public Player(String name, int id) {
 		this.name = name;
@@ -49,6 +50,9 @@ public class Player {
 	public void setTroopsAvailable(int troopsAvailable) {
 		this.troopsAvailable = troopsAvailable;
 	}
+	public void addTroopsAvailable(int number) {
+		this.troopsAvailable += number;
+	}
 	public int getSumOfAllTroops() {
 		return sumOfAllTroops;
 	}
@@ -60,12 +64,12 @@ public class Player {
 	}
 
 
-	public ArrayList<Territory> getOwnedCountries() {
+	public HashMap<CountryName, Territory> getOwnedCountries() {
 		return ownedCountries;
 	}
 
 	public void addOwnedCountries(Territory territory) {
-		this.ownedCountries.add(territory);
+		this.ownedCountries.put(territory.getCountryName(),territory);
 		territory.setOwnedByPlayer(this);
 	}
 	
@@ -73,8 +77,61 @@ public class Player {
 		return ownedContinents;
 	}
 	
-	public void updateOwnedContinents() {
-		
+	public void updateOwnedContinents(HashMap<Continent, ArrayList<Territory>> continents) {
+		 // Create a HashMap to store the number of territories owned by the player in each continent
+	    HashMap<Continent, Integer> territoriesOwnedInContinent = new HashMap<>();
+	    for (Continent continent : continents.keySet()) {
+	        territoriesOwnedInContinent.put(continent, 0);
+	    }
+	    
+	    // Count the number of territories owned by the player in each continent
+	    for (Territory territory : this.getOwnedCountries().values()) {
+	        Continent continent = territory.getContinent();
+	        int numTerritoriesOwned = territoriesOwnedInContinent.get(continent);
+	        territoriesOwnedInContinent.put(continent, numTerritoriesOwned + 1);
+	    }
+	    
+	    // Check if the player owns all territories in each continent and update the player's ownedContinents accordingly
+	    for (Continent continent : continents.keySet()) {
+	        ArrayList<Territory> territoriesInContinent = continents.get(continent);
+	        int numTerritoriesInContinent = territoriesInContinent.size();
+	        int numTerritoriesOwned = territoriesOwnedInContinent.get(continent);
+	        if (numTerritoriesOwned == numTerritoriesInContinent) {
+	            ownedContinents.add(continent);
+	        } else {
+	            ownedContinents.remove(continent);
+	        }
+	    }
+	}
+
+
+	public ArrayList<Card> getCards() {
+		return cards;
+	}
+
+
+	public void setCards(ArrayList<Card> cards) {
+		this.cards = cards;
+	}
+
+
+	public boolean isCanContinuePlaying() {
+		return canContinuePlaying;
+	}
+
+
+	public void setCanContinuePlaying(boolean stillActive) {
+		this.canContinuePlaying = stillActive;
+	}
+
+
+	public boolean isCardsTurningInPhase() {
+		return cardsTurningInPhase;
+	}
+
+
+	public void setCardsTurningInPhase(boolean cardsTurningInPhase) {
+		this.cardsTurningInPhase = cardsTurningInPhase;
 	}
 	
 }
