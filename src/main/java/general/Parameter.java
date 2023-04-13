c0683f3acc05e1a00764d75f85295b30b089fb76
Petko Mikov <pmikov@mail.uni-mannheim.de>
2023-04-13 12:46:58 +0200

    #15 added player colors and addresses for the avatar, dice, territories PNGs

--- src/main/java/general/Parameter.java
+++ src/main/java/general/Parameter.java
@@ -1,5 +1,6 @@
 package general;
 
+import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 
 /**
@@ -14,6 +15,9 @@ public class Parameter {
 	public static final String userdir = System.getProperty("user.dir");
 	public static final String resourcesdir = userdir + sep + "src" + sep + "main" + sep + "resources" + sep;
 	public static final String imagesdir = resourcesdir + "images" + sep;
+	public static final String avatarsdir = resourcesdir + "avatars" + sep;
+	public static final String dicedir = resourcesdir + "dice" + sep;
+	public static final String territoryPNGdir = resourcesdir + "territories" + sep;
 	public static final String logoImage = imagesdir + "transparent-risk.png";
 	public static final String errorIcon = imagesdir + "error_icon.png";
 	public static final String coord = resourcesdir + sep + "game" + sep + "gui" + sep + "coord.txt";
@@ -26,5 +30,18 @@ public class Parameter {
 	
 	public static final String hostDefault = "localhost";
 	public static final int portDefault = 1234;
+	
+	//player color codes
+	public static final Color redColor = Color.web("#c1272d");
+	public static final Color blueColor = Color.web("#0071bc");
+	public static final Color greenColor = Color.web("#59f23a");
+	public static final Color yellowColor = Color.web("#d9e021");
+	public static final Color orangeColor = Color.web("#f28a3a");
+	public static final Color purpleColor = Color.web("#c73af2");
+	public static final Color white = Color.web("#ffffff");
+
+
+
+
 }
 
