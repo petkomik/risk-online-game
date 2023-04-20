3ab353001532319077d891e52bb677434a0d8137
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-04-20 21:47:59 +0200

    #29 changed first Player to PlayerSingle in lobby

--- src/main/java/game/gui/MainMenuPaneController.java
+++ src/main/java/game/gui/MainMenuPaneController.java
@@ -3,34 +3,24 @@ package game.gui;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 import game.Lobby;
-import game.gui.GUISupportClasses.ArrowButton;
 import game.gui.GUISupportClasses.DesignButton;
 import game.gui.GUISupportClasses.ImageViewPane;
 import game.gui.GUISupportClasses.Spacing;
-import game.models.Player;
+import game.models.PlayerSingle;
 import general.AppController;
 import general.Parameter;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -241,7 +231,7 @@ public class MainMenuPaneController extends StackPane {
 				(new GameSound()).buttonClickForwardSound();
 				Lobby lobby = new Lobby();
 				// TODO get methods for color + avatars
-				lobby.joinLobby(new Player(AppController.getProfile().getUserName(), AppController.getProfile().getId()));
+				lobby.joinLobby(new PlayerSingle(AppController.getProfile().getUserName(), AppController.getProfile().getId()));
 				Node node = (Node) event.getSource();
 				stage = (Stage)node.getScene().getWindow();
 				try {
