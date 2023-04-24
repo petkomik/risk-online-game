bd91f4a5af51553c1aaa2740460fb0fa257f038a
Stefan Rogalski <srogalsk@mail.uni-mannheim.de>
2023-04-24 09:07:32 +0200

    #20 gameSingle added serialVersionUID

--- src/main/java/game/models/PlayerSingle.java
+++ src/main/java/game/models/PlayerSingle.java
@@ -2,6 +2,8 @@ package game.models;
 
 public class PlayerSingle extends Player {
 
+	private static final long serialVersionUID = 1L;
+
 	public PlayerSingle(String name, int id) {
 		super(name, id);
 	}
