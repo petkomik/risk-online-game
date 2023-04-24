4984c89d5795c32fa0e7b577ee3536d9caf64643
Petar Malamov <pmalamov@mail.uni-mannheim.de>
2023-04-24 23:09:13 +0200

    #41 window is now scalable to the hosts screen, added chat button GUIsup

--- src/main/java/game/gui/EndGamePodiumController.java
+++ src/main/java/game/gui/EndGamePodiumController.java
@@ -1,15 +1,21 @@
 package game.gui;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import game.gui.GUISupportClasses.ArrowButton;
+import game.gui.GUISupportClasses.ImageViewPane;
+import game.gui.GUISupportClasses.Spacing;
 import general.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
@@ -18,6 +24,8 @@ import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import javafx.stage.Popup;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /**
@@ -31,16 +39,34 @@ public class EndGamePodiumController extends Application {
 	
 	
 	public int players = 3;
+	private double ratio;
 	//public int players = game.Lobby.getPlayerList().size();
 	
-	StackPane container;	
-	HBox backgroundPic;
-	HBox backgroundColor;
-	HBox vBoxIcons;
-	HBox hBoxButton;
-	ArrowButton backbutton;
+	private	StackPane container;	
+	private HBox backgroundPic;
+	private HBox backgroundColor;
+	private VBox vBoxIcons;
+	private HBox banner;
+	private HBox topBannerContent;
+	private Label lobbyTextBanner;
+	private VBox contentVBox;
+	private ArrowButton backButton;
+	private Popup popup;
+	private ImageView imgBackground;
+	private ImageViewPane imgBackgroundPane;
+	private HBox avatars;
+	private StackPane firstP, secondP, thirdP;
+	private Circle circleFirstP, circleSecondP, circleThirdP;
+	private ImageView circleFirstI, circleSecondI, circleThirdI;
+	private HBox place;
+	private ImageView firstPlaceCup, secondPlaceCup, thirdPlaceCup;
+	private Text caption;
+	
 	
 	public EndGamePodiumController() throws Exception {
+		super();
+		this.ratio = Screen.getPrimary().getVisualBounds().getWidth() * Screen.getPrimary().getVisualBounds().getHeight() / (1846 * 1080);
+		this.ratio = Math.min(ratio + 0.3, 1);
 		this.container = this.setup();
 	}
 	
@@ -49,55 +75,97 @@ public class EndGamePodiumController extends Application {
 		
 	/*
 	 * to be returned Stackpane
-	 * background->shade->Icons->Button
+	 * background->shade->content
+	 * content consists of Icons->Button
 	 * Icons consists of 2 VBoxes
 	 * each VBox has 2-3 Parts 
 	 */
-	StackPane container = new StackPane();	
-	HBox backgroundPic = new HBox();
-	HBox backgroundColor = new HBox();
-	VBox vBoxIcons = new VBox();
-	HBox hBoxButton = new HBox();
+	container = new StackPane();
+	contentVBox = new VBox();
+	contentVBox.setAlignment(Pos.CENTER);
 	
+	vBoxIcons = new VBox();
 	vBoxIcons.setAlignment(Pos.CENTER);
-	hBoxButton.setAlignment(Pos.TOP_LEFT);
+
 	
-	
-	HBox avatars = new HBox(50);
-	HBox place = new HBox(60);
+	avatars = new HBox(50*ratio);
+	avatars.setAlignment(Pos.CENTER);
+	place = new HBox(60*ratio);
+	place.setAlignment(Pos.CENTER);
 	
 	/*
 	 * setting background Image and color
 	 */
+	backgroundPic = new HBox();
+	backgroundColor = new HBox();
 	
-	ImageView imgBackground = new ImageView();
+	imgBackground = new ImageView();
 	imgBackground.setImage(new Image(new FileInputStream(Parameter.imagesdir + "world-map.png")));
 	imgBackground.setPreserveRatio(false);
 	imgBackground.setSmooth(true);
 	imgBackground.setCache(true);
 	
-	backgroundPic.getChildren().add(imgBackground);
+	imgBackgroundPane = new ImageViewPane(imgBackground);
+	HBox.setHgrow(imgBackgroundPane, Priority.ALWAYS);
+	backgroundPic.getChildren().add(imgBackgroundPane);
 	
 	backgroundColor.setAlignment(Pos.CENTER);
 	backgroundColor.setFillHeight(true);
 	backgroundColor.setStyle("-fx-background-color: rgba(225, 211, 184, 0.9);");
 	
+	/*
+	 * setting up banner layer
+	 */
+	
+	banner = new HBox(); 
+	banner.setAlignment(Pos.TOP_LEFT);
+	VBox.setMargin(banner, new Insets(50 * ratio,0,0,-15 * ratio));
+	banner.setPickOnBounds(false);
+
+	topBannerContent = new HBox();
+	topBannerContent.setAlignment(Pos.CENTER);
+	topBannerContent.setStyle("-fx-background-color: "
+			+ "linear-gradient(to right, rgba(100, 68, 31, 1) 60%, "
+			+ "rgba(100, 68, 31, 0.7) 75%, rgba(100, 68, 31, 0) 95%);");
+	topBannerContent.setMaxWidth(800 * ratio);
+	topBannerContent.setMinWidth(500 * ratio);
+	topBannerContent.setPadding(new Insets(10 * ratio, 150 * ratio, 10 * ratio, 30 * ratio));
+	topBannerContent.minHeightProperty().bind(topBannerContent.maxHeightProperty());
+	topBannerContent.maxHeightProperty().bind(topBannerContent.prefHeightProperty());
+	topBannerContent.setPrefHeight(100 * ratio);
+	HBox.setHgrow(topBannerContent, Priority.ALWAYS);
+	
+	backButton = new ArrowButton(60 * ratio);
+	
+	Spacing bannerContentSpacing = new Spacing();
+	HBox.setHgrow(bannerContentSpacing, Priority.ALWAYS);
+	
+	lobbyTextBanner = new Label("TOP PLAYERS");
+	lobbyTextBanner.setFont(Font.font("Cooper Black", FontWeight.NORMAL, 60 * ratio));
+	lobbyTextBanner.setTextFill(Color.WHITE);
+	
+	Spacing bannerSpacing = new Spacing();
+	HBox.setHgrow(bannerSpacing, Priority.ALWAYS);
+	bannerSpacing.setVisible(false);
+	
+	topBannerContent.getChildren().addAll(backButton, bannerContentSpacing, lobbyTextBanner);
+	banner.getChildren().addAll(topBannerContent, bannerSpacing);
 	
 	
 	/*
 	 * setting up players avatars - in the VBox avatars
 	 */
 	
-	StackPane firstP= new StackPane();
-	StackPane secondP = new StackPane();
-	StackPane thirdP = new StackPane();
+	firstP= new StackPane();
+	secondP = new StackPane();
+	thirdP = new StackPane();
 		
 	/*
 	 * image and shape for the first avatar
 	 */
 	
-	Circle circleFirstP = new Circle(90);
-	ImageView circleFirstI = new ImageView();
+	circleFirstP = new Circle(90*ratio);
+	circleFirstI = new ImageView();
 	//TODO set up the right color
 	circleFirstP.setFill(Parameter.blueColor);
 	circleFirstP.setStroke(Color.WHITE);
@@ -106,8 +174,8 @@ public class EndGamePodiumController extends Application {
 	firstP.getChildren().add(circleFirstP);
 	
 	circleFirstI.setImage(new Image(new FileInputStream(Parameter.blondBoy)));
-	circleFirstI.setFitWidth(200);
-	circleFirstI.setFitHeight(200);
+	circleFirstI.setFitWidth(200*ratio);
+	circleFirstI.setFitHeight(200*ratio);
 	circleFirstI.setPreserveRatio(true);
 	circleFirstI.setSmooth(true);
 	circleFirstI.setCache(true);	
@@ -119,8 +187,8 @@ public class EndGamePodiumController extends Application {
 	 * image and shape for the second avatar
 	 */
 	
-	Circle circleSecondP = new Circle(80);
-	ImageView circleSecondI = new ImageView();
+	circleSecondP = new Circle(80*ratio);
+	circleSecondI = new ImageView();
 	//TODO set up the right color
 	circleSecondP.setFill(Parameter.greenColor);
 	circleSecondP.setStroke(Color.WHITE);
@@ -129,8 +197,8 @@ public class EndGamePodiumController extends Application {
 	secondP.getChildren().add(circleSecondP);
 	
 	circleSecondI.setImage(new Image(new FileInputStream(Parameter.hatBoy)));
-	circleSecondI.setFitWidth(170);
-	circleSecondI.setFitHeight(170);
+	circleSecondI.setFitWidth(170*ratio);
+	circleSecondI.setFitHeight(170*ratio);
 	circleSecondI.setPreserveRatio(true);
 	circleSecondI.setSmooth(true);
 	circleSecondI.setCache(true);
@@ -142,8 +210,8 @@ public class EndGamePodiumController extends Application {
 	 * image and shape for the third avatar
 	 */
 	
-	Circle circleThirdP = new Circle(70);
-	ImageView circleThirdI = new ImageView();
+	circleThirdP = new Circle(70*ratio);
+	circleThirdI = new ImageView();
 	//TODO set up the right color
 	if(players>2) {
 	circleThirdP.setFill(Parameter.yellowColor);
@@ -153,8 +221,8 @@ public class EndGamePodiumController extends Application {
 	thirdP.getChildren().add(circleThirdP);
 	
 	circleThirdI.setImage(new Image(new FileInputStream(Parameter.gingerGirl)));
-	circleThirdI.setFitWidth(140);
-	circleThirdI.setFitHeight(140);
+	circleThirdI.setFitWidth(140*ratio);
+	circleThirdI.setFitHeight(140*ratio);
 	circleThirdI.setPreserveRatio(true);
 	circleThirdI.setSmooth(true);
 	circleThirdI.setCache(true);
@@ -170,34 +238,34 @@ public class EndGamePodiumController extends Application {
 	}else {
 		avatars.getChildren().addAll(firstP,secondP);
 	}
-	avatars.setPadding(new Insets(20,500,20,500));
+	avatars.setPadding(new Insets(20,200*ratio,20,200*ratio));
 	 
 	/*
 	 * setting up the images for the cups 
 	 */
 	
-	ImageView firstPlaceCup = new ImageView();
+	firstPlaceCup = new ImageView();
 	firstPlaceCup.setImage(new Image(new FileInputStream(Parameter.podiumdir + "PodiumPlace1.png")));
-	firstPlaceCup.setFitWidth(200);
-	firstPlaceCup.setFitHeight(200);
+	firstPlaceCup.setFitWidth(200*ratio);
+	firstPlaceCup.setFitHeight(200*ratio);
 	firstPlaceCup.setPreserveRatio(true);
 	firstPlaceCup.setSmooth(true);
 	firstPlaceCup.setCache(true);
 	
-	ImageView secondPlaceCup = new ImageView();
+	secondPlaceCup = new ImageView();
 	secondPlaceCup.setImage(new Image(new FileInputStream(Parameter.podiumdir + "PodiumPlace2.png")));
-	secondPlaceCup.setFitWidth(170);
-	secondPlaceCup.setFitHeight(170);
+	secondPlaceCup.setFitWidth(170*ratio);
+	secondPlaceCup.setFitHeight(170*ratio);
 	secondPlaceCup.setPreserveRatio(true);
 	secondPlaceCup.setSmooth(true);
 	secondPlaceCup.setCache(true);
 	
-	ImageView thirdPlaceCup = new ImageView();
+	thirdPlaceCup = new ImageView();
 	
 	if(players>2) {
 	thirdPlaceCup.setImage(new Image(new FileInputStream(Parameter.podiumdir + "PodiumPlace3.png")));
-	thirdPlaceCup.setFitWidth(140);
-	thirdPlaceCup.setFitHeight(140);
+	thirdPlaceCup.setFitWidth(140*ratio);
+	thirdPlaceCup.setFitHeight(140*ratio);
 	thirdPlaceCup.setPreserveRatio(true);
 	thirdPlaceCup.setSmooth(true);
 	thirdPlaceCup.setCache(true);
@@ -212,48 +280,46 @@ public class EndGamePodiumController extends Application {
 	}else {
 		place.getChildren().addAll(firstPlaceCup, secondPlaceCup);
 	}
-	place.setPadding(new Insets(20,500,20,500));
+	place.setPadding(new Insets(20*ratio,200*ratio,20*ratio,200*ratio));
 	
 	/*
 	 * adding endgame text
 	 */
-	Text text = new Text("WINNER");
-	text.setFont(Font.font("Cooper Black", FontWeight.NORMAL, 80));
-	text.setFill(Color.GOLD);
-	text.setStroke(Color.WHITE);
-	text.setStrokeWidth(2.0);
+	caption = new Text("WINNER");
+	caption.setFont(Font.font("Cooper Black", FontWeight.NORMAL, 100*ratio));
+	caption.setFill(Color.GOLD);
+	caption.setStroke(Color.WHITE);
+	caption.setStrokeWidth(2.0);
 
 	/*
 	 * adding the cups and avatars to the hBox
 	 */
 	
-	vBoxIcons.getChildren().addAll(text,avatars, place);
-	
-	/*
-	 * setting up back button
-	 */
-	
-	ArrowButton backButton = new ArrowButton(60);
-	hBoxButton.getChildren().add(backButton);
-	hBoxButton.setPadding(new Insets(20,20,20,20));
+	vBoxIcons.getChildren().addAll(caption,avatars, place);
 	
 	/*
 	 * Action handler for the button
 	 */
 	
+
 	backButton.setOnAction(new EventHandler<ActionEvent>() {
 		@Override
 		public void handle(ActionEvent event) {
-		(new GameSound()).buttonClickForwardSound();
-		//TODO has to return the player to the ServerMainWindow
+			(new GameSound()).buttonClickForwardSound();
+			//TODO has to return the player to the ServerMainWindow
+
 		}
 	});
 	
 	/*
 	 * adding everything to the top container
 	 */
+	contentVBox.setAlignment(Pos.CENTER);
+	contentVBox.setSpacing(30*ratio);
+	contentVBox.setMaxWidth(Screen.getPrimary().getVisualBounds().getWidth());
 	
-	container.getChildren().addAll(backgroundPic, backgroundColor, vBoxIcons, hBoxButton);	
+	contentVBox.getChildren().addAll(banner,new Spacing(50),vBoxIcons,new Spacing(100));
+	container.getChildren().addAll(backgroundPic, backgroundColor, contentVBox);	
 	
 	return container;
 	}
@@ -269,6 +335,7 @@ public class EndGamePodiumController extends Application {
 		primaryStage.setMinHeight(800);
 		primaryStage.setMinWidth(1300);
 		primaryStage.show();
+		
 		
 	}
 	
--- src/main/java/game/gui/GUISupportClasses.java
+++ src/main/java/game/gui/GUISupportClasses.java
@@ -17,6 +17,7 @@ import javafx.geometry.VPos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -162,6 +163,7 @@ public class GUISupportClasses {
 		}
 	}
 	
+	
 	static class ArrowButton extends Button {
 		public ArrowButton() {
 			super();
@@ -215,6 +217,57 @@ public class GUISupportClasses {
 			//inset = Math.min(1, inset + 0.05);
 			this.setPadding(new Insets(-inset, 0, 0, 0));
 		}
+	}
+	
+	static class ChatButton extends ToggleButton {
+		
+		public ChatButton() {
+			super();
+			this.setText("\uD83D\uDDE8");
+			this.setAlignment(Pos.CENTER_LEFT);
+			this.setFont(Font.font("Segoe UI Emoji", FontWeight.BOLD, 60));
+			this.setMaxSize(60, 60);
+			this.setMinSize(60, 60);
+			this.setPrefSize(60, 60);
+			this.setAlignment(Pos.BASELINE_CENTER);
+			this.setPadding(new Insets(0, 0, 0, 0));
+			this.setSelected(false);
+
+			this.setTextFill(Color.WHITE);
+			this.setStyle("-fx-background-color: #b87331;"
+					+ "-fx-background-insets: 1 1 1 1;"
+					+ "-fx-background-radius: 12;"
+					+ "-fx-border-radius: 12;"
+	       			+ "-fx-border-color: #b87331;"
+	    			+ "-fx-border-width: 3px;");
+			
+			this.hoverProperty().addListener((ChangeListener<Boolean>) (observable, oldValue, newValue) -> {
+	            if (newValue) {
+	            	this.setStyle("-fx-background-color: #b87331;"
+	    					+ "-fx-background-insets: 1 1 1 1;"
+	        				+ "-fx-background-radius: 12;"
+	        				+ "-fx-border-radius: 12;"
+	            			+ "-fx-border-color: #ffffff;"
+	            			+ "-fx-border-width: 3px;");
+	            } else {
+	            	this.setStyle("-fx-background-color: #b87331;"
+	    					+ "-fx-background-insets: 1 1 1 1;"
+	        				+ "-fx-background-radius: 12;"
+	        				+ "-fx-border-radius: 12;"
+	            			+ "-fx-border-color: #b87331;"
+	            			+ "-fx-border-width: 3px;");
+	            }
+	        });
+		}
+		
+		public ChatButton(double k) {
+			this();
+			this.setFont(Font.font("Segoe UI Emoji", FontWeight.BOLD, k/3));
+			this.setMaxSize(k, k);
+			this.setMinSize(k, k);
+			this.setPrefSize(k, k);
+		}
+
 	}
 	
 	static class PlayerCard extends VBox {
