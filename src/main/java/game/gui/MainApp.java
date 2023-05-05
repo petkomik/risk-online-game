af7242106d299fd02ef2b5daf98c87987906a272
Danail Ignatovski <dignatov@mail.uni-mannheim.de>
2023-05-05 14:24:25 +0200

    #38 client!=null MainAPP

--- src/main/java/game/gui/MainApp.java
+++ src/main/java/game/gui/MainApp.java
@@ -21,76 +21,83 @@ import network.messages.MessageServerCloseConnection;
 
 /**
  * 
- * @author malaji
- * This class shows the very first page
+ * @author malaji This class shows the very first page
  */
-public class MainApp extends Application{
-	
+public class MainApp extends Application {
+
 	public static double screenWidth;
 	public static double screenHeight;
 	private AnchorPane anchorPane;
-    private Scene scene;
-    public static GameSound themeSound;
-    public static AppController appController;
-    /**
-     * This method returns the GameController instance
-     * @return gameController 
-     */
-    public static AppController getAppController() {
-    	MainApp.appController = AppController.getInstance();
-    	return MainApp.appController;
-    }
-    
-    /**
-     * This method initializes the required elements
-     */
-    @Override
-    public void start(Stage stage) throws IOException {
-    	screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
-    	screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
-        StartPaneController start = new StartPaneController();
-        start.setPrefSize(MainApp.screenWidth, MainApp.screenHeight);
-        scene = new Scene(start);
-        Image image = new Image(Parameter.dicesIcon);
-        stage.getIcons().add(image);
-        stage.setScene(scene);
-        stage.setMaximized(true);
-        stage.setResizable(true);
-        stage.setMinHeight(600);
+	private Scene scene;
+	public static GameSound themeSound;
+	public static AppController appController;
+
+	/**
+	 * This method returns the GameController instance
+	 * 
+	 * @return gameController
+	 */
+	public static AppController getAppController() {
+		MainApp.appController = AppController.getInstance();
+		return MainApp.appController;
+	}
+
+	/**
+	 * This method initializes the required elements
+	 */
+	@Override
+	public void start(Stage stage) throws IOException {
+		screenWidth = Screen.getPrimary().getVisualBounds().getWidth();
+		screenHeight = Screen.getPrimary().getVisualBounds().getHeight();
+		StartPaneController start = new StartPaneController();
+		start.setPrefSize(MainApp.screenWidth, MainApp.screenHeight);
+		scene = new Scene(start);
+		Image image = new Image(Parameter.dicesIcon);
+		stage.getIcons().add(image);
+		stage.setScene(scene);
+		stage.setMaximized(true);
+		stage.setResizable(true);
+		stage.setMinHeight(600);
 		stage.setMinWidth(900);
 		stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-			
-		@Override
-		public void handle(WindowEvent event) {
-			
-			Client client = AppController.getClient();
-			if(client.isHost()){
-				client.sendMessage(new MessageServerCloseConnection());
-			}else{
-				client.sendMessage(new MessageDisconnect(client.getProfile()));
-			}
-			Platform.exit();
-			System.exit(0);
-		}
-		});
-        stage.show();
-        
-        /**************** Sound *************************//**
-        File file = new File(Parameter.themeSong);
-		Media media = new Media(file.toURI().toString());
-		System.out.println(file.toURI().toString());
-		MediaPlayer mediaPlayer = new MediaPlayer(media);
-		mediaPlayer.play();*/
-        
-        themeSound = new GameSound();
-        themeSound.startThemeSong(); 
-		
-    }
 
-    /**
-     * this class method enables the launching of the App outside the class
-     * @param args
-     */
+			@Override
+			public void handle(WindowEvent event) {
+
+				Client client = AppController.getClient();
+				if (client.isHost()) {
+					if ((client != null)) {
+						client.sendMessage(new MessageServerCloseConnection());
+					}
+				} else {
+					if (client != null) {
+
+						client.sendMessage(new MessageDisconnect(client.getProfile()));
+					}
+				}
+				Platform.exit();
+				System.exit(0);
+			}
+		});
+		stage.show();
+
+		/**************** Sound *************************//**
+															 * File file = new File(Parameter.themeSong); Media media =
+															 * new Media(file.toURI().toString());
+															 * System.out.println(file.toURI().toString()); MediaPlayer
+															 * mediaPlayer = new MediaPlayer(media); mediaPlayer.play();
+															 */
+
+		themeSound = new GameSound();
+		themeSound.startThemeSong();
+
+	}
+
+	/**
+	 * this class method enables the launching of the App outside the class
+	 * 
+	 * @param args
+	 */
 	public static void runMainApp(String[] args) {
 		launch(args);
 	}
