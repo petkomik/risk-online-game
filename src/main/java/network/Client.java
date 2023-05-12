9ab6185a940267a04f446b78907b82450ff936a9
Danail Ignatovski <dignatov@mail.uni-mannheim.de>
2023-05-12 15:13:21 +0200

    #38 sdadsa

--- src/main/java/network/Client.java
+++ src/main/java/network/Client.java
@@ -272,12 +272,12 @@ public class Client {
 								AppController.dbH.createProfileData(profilee);
 							}
 							 // if is person
-							AppController.dbH.updateProfileInfo(
-				                      AppController.dbH.getProfileByID((profilee.getId())).getWins() , "Wins",
-				                      profilee.getId());
-							AppController.dbH.updateProfileInfo(
-				                      AppController.dbH.getProfileByID((profilee.getId())).getLoses() , "Loses",
-				                      profilee.getId());
+//							AppController.dbH.updateProfileInfo(
+//				                      AppController.dbH.getProfileByID((profilee.getId())).getWins() , "Wins",
+//				                      profilee.getId());
+//							AppController.dbH.updateProfileInfo(
+//				                      AppController.dbH.getProfileByID((profilee.getId())).getLoses() , "Loses",
+//				                      profilee.getId());
 							 
 							
 							profiles.add(profilee);
@@ -353,12 +353,12 @@ public class Client {
 								AppController.dbH.createProfileData(profilee1);
 								System.out.println("profileadding works");
 							}
-							AppController.dbH.updateProfileInfo(
-				                      AppController.dbH.getProfileByID((profilee1.getId())).getWins() , "Wins",
-				                      profilee1.getId());
-							AppController.dbH.updateProfileInfo(
-				                      AppController.dbH.getProfileByID((profilee1.getId())).getLoses() , "Loses",
-				                      profilee1.getId());
+//							AppController.dbH.updateProfileInfo(
+//				                      AppController.dbH.getProfileByID((profilee1.getId())).getWins() , "Wins",
+//				                      profilee1.getId());
+//							AppController.dbH.updateProfileInfo(
+//				                      AppController.dbH.getProfileByID((profilee1.getId())).getLoses() , "Loses",
+//				                      profilee1.getId());
 							chat.addItemsInComboBox(profilee1);
 							System.out.println("MessageProfile");
 							break;
